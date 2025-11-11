f3a5dfa377ef31cab537be9f0d8fbed685f8966d
Tyr-64 <vcas47@gmail.com>
2025-11-10 22:10:07 -0500

    Update script.js

--- projects/artJam/js/script.js
+++ projects/artJam/js/script.js
@@ -31,7 +31,6 @@ let button = {
 
 let blendType = 0;
 
-
 let display = {
     pics: [],
     x: 550,
@@ -57,7 +56,7 @@ let hand = {
 }
 
 // preload image assets
-function preload(){
+function preload() {
     hand.img1 = loadImage('./assets/images/left.png');
     hand.img2 = loadImage('./assets/images/middle.png');
     hand.img3 = loadImage('./assets/images/right.png');
@@ -87,7 +86,7 @@ function preload(){
 
 // set canvas and background, disable cursor visibility
 function setup() {
-    createCanvas(800,500);
+    createCanvas(800, 500);
     background("#000000ff");
     noCursor();
 }
@@ -98,10 +97,10 @@ function setup() {
 */
 function draw() {
     background("#000000ff");
-    if(i == 14){
+    if (i == 14) {
         setTimeout(finalScreen, 1500);
     }
-    image(bg.main, 0,0);
+    image(bg.main, 0, 0);
     buttonPress();
     imgDisplay();
     button.up = false;
@@ -111,77 +110,76 @@ function draw() {
     button.left = false;
     button.frown = false;
     //console.log(mouseX, mouseY);
-    if(display.rightActive == true){
+    if (display.rightActive == true) {
         image(display.right, 631, 82, 164, 216);
     }
-    if(display.leftActive == true){
+    if (display.leftActive == true) {
         image(display.left, 3, 82, 164, 216);
     }
     drawHand();
-    
 }
 
 // Draws hand image depending on location, uses y value to create a depth effect
-function drawHand () {
+function drawHand() {
     let sizeMult = map(mouseY, 300, 500, 0.48, 1.8, true);
-    if (sizeMult == 0){
+    if (sizeMult == 0) {
         sizeMult += 0.1;
     }
     hand.size = sizeMult * 200;
     hand.y = constrain(mouseY, 50, 400);
-    hand.x = constrain(mouseX - (50*sizeMult), 0, 800 - (100*sizeMult));
+    hand.x = constrain(mouseX - (50 * sizeMult), 0, 800 - (100 * sizeMult));
     if (mouseX <= 300) {
-        image(hand.img1, hand.x-25, hand.y, hand.size, hand.size);
+        image(hand.img1, hand.x - 25, hand.y, hand.size, hand.size);
     }
-    else if (mouseX <= 500){
-        image(hand.img2, hand.x-25, hand.y, hand.size, hand.size);
+    else if (mouseX <= 500) {
+        image(hand.img2, hand.x - 25, hand.y, hand.size, hand.size);
     }
     else {
-        image(hand.img3, hand.x-25, hand.y, hand.size, hand.size);
+        image(hand.img3, hand.x - 25, hand.y, hand.size, hand.size);
     }
 }
 
 // checks location of pointer to corresponding button on screen. changes background image and sets button boolean to true (reversed in draw)
-function buttonPress(){
+function buttonPress() {
     // up arrow
-    if (hand.x >= 506 && hand.x <= 593 && hand.y >=251 && hand.y <= 300 && display.y > 0){
+    if (hand.x >= 506 && hand.x <= 593 && hand.y >= 251 && hand.y <= 300 && display.y > 0) {
         image(bg.up, 0, 0);
         button.up = true;
     }
     // right arrow
-    else if (hand.x >= 506 && hand.x <= 593 && hand.y >=168 && hand.y <= 216 && display.x < 994){
+    else if (hand.x >= 506 && hand.x <= 593 && hand.y >= 168 && hand.y <= 216 && display.x < 994) {
         image(bg.right, 0, 0);
         button.right = true;
     }
     // smiley :)
-    else if (hand.x >= 506 && hand.x <= 593 && hand.y >=85 && hand.y <= 137){
+    else if (hand.x >= 506 && hand.x <= 593 && hand.y >= 85 && hand.y <= 137) {
         image(bg.smile, 0, 0);
         button.smile = true;
     }
     //down arrow
-    else if (hand.x >= 168 && hand.x <= 250 && hand.y >=251 && hand.y <= 300 && display.y < 930){
+    else if (hand.x >= 168 && hand.x <= 250 && hand.y >= 251 && hand.y <= 300 && display.y < 930) {
         image(bg.down, 0, 0);
         button.down = true;
     }
     // left arrow
-    else if (hand.x >= 168 && hand.x <= 250 && hand.y >=168 && hand.y <= 216 && display.x > 0){
+    else if (hand.x >= 168 && hand.x <= 250 && hand.y >= 168 && hand.y <= 216 && display.x > 0) {
         image(bg.left, 0, 0);
         button.left = true;
     }
     // frown :(
-    else if (hand.x >= 168 && hand.x <= 250 && hand.y >=85 && hand.y <= 137){
+    else if (hand.x >= 168 && hand.x <= 250 && hand.y >= 85 && hand.y <= 137) {
         image(bg.frown, 0, 0);
         button.frown = true;
     }
 }
 
 //using the button booleans, image display either moves or cycles the images, then calls on the blend functions to alter/place images on the right and left
-function imgDisplay(){
+function imgDisplay() {
     //displays image using i iterator variable, cropping it and placing it in the center with p5's image arguments. display x and y are the values that allow the image to 'move' under the crop.
-    image(display.pics[i],293,55, 205, 270, display.x, display.y, 205, 270);
+    image(display.pics[i], 293, 55, 205, 270, display.x, display.y, 205, 270);
     display.transfer = display.pics[i].get(display.x, display.y, 205, 270);
     //start with the up down left right arrows, moving the current center image
-    if (button.up == true){
+    if (button.up == true) {
         display.y -= 2;
         console.log(display.y);
     }
@@ -189,100 +187,100 @@ function imgDisplay(){
         display.y += 2;
         console.log(display.y);
     }
-    else if(button.left == true){
+    else if (button.left == true) {
         display.x -= 2;
         console.log(display.x);
     }
-    else if(button.right == true){
+    else if (button.right == true) {
         display.x += 2;
         console.log(display.x);
     }
     // then check if one of the face buttons was pressed and act accordingly
-    else if(button.smile == true && delay == false){
-        
-        if(display.rightActive == true){
+    else if (button.smile == true && delay == false) {
+
+        if (display.rightActive == true) {
             blendRight();
         }
-        else if(display.rightActive == false){
+        else if (display.rightActive == false) {
             display.right = display.transfer;
         }
         display.rightActive = true;
-        i ++;
+        i++;
         delay = true;
         setTimeout(nextPic, 1000);
     }
-    else if(button.frown == true && delay == false){
-        if(display.leftActive == true){
+    else if (button.frown == true && delay == false) {
+        if (display.leftActive == true) {
             blendLeft();
         }
-        else if(display.leftActive == false){
+        else if (display.leftActive == false) {
             display.left = display.transfer;
         }
         display.leftActive = true;
-        i ++;
+        i++;
         delay = true;
         setTimeout(nextPic, 1000);
     }
 }
 //toggle for boolean in delay function, keeps the user from blasting through all the images with a single button press
-function nextPic(){
+function nextPic() {
     delay = false;
 }
 // blends images for right and left side using a random blend mode corresponding to each side
 // this feels like a convoluted way of doing it, but using a string variable wasn't working as an argument in the blend function so here we are ¯\_(-.-)_/¯
-function blendRight(){
-    blendType = int(random(1,5));
-    if (blendType == 1){
+function blendRight() {
+    blendType = int(random(1, 5));
+    if (blendType == 1) {
         //takes the image on the right and combines it with the image from the center held in the display.transfer variable, check the p5 image blend reference for details
-        display.right.blend(display.transfer,0,0,205, 270,0,0,205,270, HARD_LIGHT);
+        display.right.blend(display.transfer, 0, 0, 205, 270, 0, 0, 205, 270, HARD_LIGHT);
         //if you want to know how the images are being combined then uncomment these console.log lines :-)
         //console.log("hard light");
     }
-    else if (blendType == 2){
-        display.right.blend(display.transfer,0,0,205, 270,0,0,205,270, SOFT_LIGHT);
+    else if (blendType == 2) {
+        display.right.blend(display.transfer, 0, 0, 205, 270, 0, 0, 205, 270, SOFT_LIGHT);
         //console.log("soft light");
     }
-    else if (blendType == 3){
-        display.right.blend(display.transfer,0,0,205, 270,0,0,205,270, ADD);
+    else if (blendType == 3) {
+        display.right.blend(display.transfer, 0, 0, 205, 270, 0, 0, 205, 270, ADD);
         //console.log("add");
     }
-    else if (blendType == 4){
-        display.right.blend(display.transfer,0,0,205, 270,0,0,205,270, LIGHTEST);
+    else if (blendType == 4) {
+        display.right.blend(display.transfer, 0, 0, 205, 270, 0, 0, 205, 270, LIGHTEST);
         //console.log("lightest");
     }
-    else if (blendType == 5){
-        display.right.blend(display.transfer,0,0,205, 270,0,0,205,270, MULTIPLY);
+    else if (blendType == 5) {
+        display.right.blend(display.transfer, 0, 0, 205, 270, 0, 0, 205, 270, MULTIPLY);
         //console.log("multiply");
     }
 }
 //(i know that the right side is biased towards making bright or even all white images, but i kind of like it that way)
 
 // same deal. other side. different blend modes.
-function blendLeft(){
-    blendType = int(random(1,5));
-    if (blendType == 1){
-        display.left.blend(display.transfer,0,0,205, 270,0,0,205,270, DARKEST);
+function blendLeft() {
+    blendType = int(random(1, 5));
+    if (blendType == 1) {
+        display.left.blend(display.transfer, 0, 0, 205, 270, 0, 0, 205, 270, DARKEST);
         //console.log("darkest");
     }
-    else if (blendType == 2){
-        display.left.blend(display.transfer,0,0,205, 270,0,0,205,270, BURN);
+    else if (blendType == 2) {
+        display.left.blend(display.transfer, 0, 0, 205, 270, 0, 0, 205, 270, BURN);
         //console.log("burn");
     }
-    else if (blendType == 3){
-        display.left.blend(display.transfer,0,0,205, 270,0,0,205,270, DODGE);
+    else if (blendType == 3) {
+        display.left.blend(display.transfer, 0, 0, 205, 270, 0, 0, 205, 270, DODGE);
         //console.log("dodge");
     }
-    else if (blendType == 4){
-        display.left.blend(display.transfer,0,0,205, 270,0,0,205,270, DIFFERENCE);
+    else if (blendType == 4) {
+        display.left.blend(display.transfer, 0, 0, 205, 270, 0, 0, 205, 270, DIFFERENCE);
         //console.log("difference");
     }
-    else if (blendType == 5){
-        display.left.blend(display.transfer,0,0,205, 270,0,0,205,270, SCREEN);
+    else if (blendType == 5) {
+        display.left.blend(display.transfer, 0, 0, 205, 270, 0, 0, 205, 270, SCREEN);
         //console.log("screen");
     }
 }
 // a nice gallery view of your works :D - draws the end screen then allows the code to break a bit later, making this the last rendered frame
-function finalScreen(){
+function finalScreen() {
     background("#ffffffff");
     image(display.left, 30, 40, 328, 432);
     image(display.right, 430, 40, 328, 432);
