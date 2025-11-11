de3cb31bb8f6343ce78fb6a36571da89e945f687
Tyr-64 <vcas47@gmail.com>
2025-11-10 22:02:36 -0500

    Finished blending system, added end screen, and finalized code
    Blending system finished! And added a nice end screen to get a closer look at the final images.
    
    Added comments for clarity, fixed left hand image, cleaned up code (like removing distance function, which was easily combined with drawHand)

--- projects/artJam/js/script.js
+++ projects/artJam/js/script.js
@@ -9,12 +9,6 @@
 /**
  * Establish scene objects, functions, and preload images
 */
-const ball = {
-    x: 400,
-    y: 250,
-    color: "#0d00ffff",
-    size: 100
-};
 
 let bg = {
     main: undefined,
@@ -35,7 +29,7 @@ let button = {
     frown: false
 }
 
-let blendRight = ["BLEND", "HARD_LIGHT", "SOFT_LIGHT", "ADD", "LIGHTEST", "MULTIPLY"];
+let blendType = 0;
 
 
 let display = {
@@ -48,19 +42,10 @@ let display = {
     rightActive: false,
     leftActive: false,
 }
-
-let i = 1;
+//image array iterator
+let i = 0;
 
 let delay = false;
-
-
-const hole = {
-    x: 500,
-    y: 100,
-    color: "#000000",
-    xSize: 100,
-    ySize: 50
-}
 
 let hand = {
     x: undefined,
@@ -70,6 +55,7 @@ let hand = {
     img2: undefined,
     img3: undefined,
 }
+
 // preload image assets
 function preload(){
     hand.img1 = loadImage('./assets/images/left.png');
@@ -82,44 +68,42 @@ function preload(){
     bg.frown = loadImage('./assets/images/bgfrown.jpg');
     bg.left = loadImage('./assets/images/bgarrowleft.jpg');
     bg.right = loadImage('./assets/images/bgarrowright.jpg');
-    display.pics[1] = loadImage('./assets/images/1.jpg');
-    display.pics[2] = loadImage('./assets/images/2.jpg');
-    display.pics[3] = loadImage('./assets/images/3.jpg');
-    display.pics[4] = loadImage('./assets/images/4.jpg');
-    display.pics[5] = loadImage('./assets/images/5.jpg');
-    display.pics[6] = loadImage('./assets/images/6.jpg');
-    display.pics[7] = loadImage('./assets/images/7.jpg');
-    display.pics[8] = loadImage('./assets/images/8.jpg');
-    display.pics[9] = loadImage('./assets/images/9.jpg');
-    display.pics[10] = loadImage('./assets/images/10.jpg');
-    display.pics[11] = loadImage('./assets/images/11.jpg');
-    display.pics[12] = loadImage('./assets/images/12.jpg');
-    display.pics[13] = loadImage('./assets/images/13.jpg');
-    display.pics[14] = loadImage('./assets/images/14.jpg');
-    display.pics[15] = loadImage('./assets/images/15.jpg');
-
-
-
+    display.pics[0] = loadImage('./assets/images/1.jpg');
+    display.pics[1] = loadImage('./assets/images/2.jpg');
+    display.pics[2] = loadImage('./assets/images/3.jpg');
+    display.pics[3] = loadImage('./assets/images/4.jpg');
+    display.pics[4] = loadImage('./assets/images/5.jpg');
+    display.pics[5] = loadImage('./assets/images/6.jpg');
+    display.pics[6] = loadImage('./assets/images/7.jpg');
+    display.pics[7] = loadImage('./assets/images/8.jpg');
+    display.pics[8] = loadImage('./assets/images/9.jpg');
+    display.pics[9] = loadImage('./assets/images/10.jpg');
+    display.pics[10] = loadImage('./assets/images/11.jpg');
+    display.pics[11] = loadImage('./assets/images/12.jpg');
+    display.pics[12] = loadImage('./assets/images/13.jpg');
+    display.pics[13] = loadImage('./assets/images/14.jpg');
+    display.pics[14] = loadImage('./assets/images/15.jpg');
 }
 
-// set canvas and background
+// set canvas and background, disable cursor visibility
 function setup() {
     createCanvas(800,500);
     background("#000000ff");
-    //noCursor();
+    noCursor();
 }
 
 
 /**
- * Draw image elements
+ * Draw image elements like hand, background, and images as well as reset button booleans so they dont stay on after being depressed
 */
 function draw() {
     background("#000000ff");
+    if(i == 14){
+        setTimeout(finalScreen, 1500);
+    }
     image(bg.main, 0,0);
     buttonPress();
     imgDisplay();
-    drawHand();
-    noCursor();
     button.up = false;
     button.right = false;
     button.smile = false;
@@ -128,16 +112,24 @@ function draw() {
     button.frown = false;
     //console.log(mouseX, mouseY);
     if(display.rightActive == true){
-        image(display.right,0 ,0);
+        image(display.right, 631, 82, 164, 216);
     }
+    if(display.leftActive == true){
+        image(display.left, 3, 82, 164, 216);
+    }
+    drawHand();
     
 }
 
-// Draw hand image depending on location, scaling with height to create a depth effect
+// Draws hand image depending on location, uses y value to create a depth effect
 function drawHand () {
-    hand.size = distance(mouseY) * 200;
+    let sizeMult = map(mouseY, 300, 500, 0.48, 1.8, true);
+    if (sizeMult == 0){
+        sizeMult += 0.1;
+    }
+    hand.size = sizeMult * 200;
     hand.y = constrain(mouseY, 50, 400);
-    hand.x = constrain(mouseX - (50*distance(mouseY)), 0, 800 - (100*distance(mouseY)));
+    hand.x = constrain(mouseX - (50*sizeMult), 0, 800 - (100*sizeMult));
     if (mouseX <= 300) {
         image(hand.img1, hand.x-25, hand.y, hand.size, hand.size);
     }
@@ -148,14 +140,8 @@ function drawHand () {
         image(hand.img3, hand.x-25, hand.y, hand.size, hand.size);
     }
 }
-// calculates a 'distance' value from the height of a hand, returning a value from 0 to 1 with a cutoff on the horizon 300 pixels below the top of the frame
-function distance(Ypos){
-    let sizeMult = map(mouseY, 300, 500, 0.48, 1.8, true);
-    if (sizeMult == 0){
-        sizeMult += 0.1;
-    }
-    return sizeMult;
-}
+
+// checks location of pointer to corresponding button on screen. changes background image and sets button boolean to true (reversed in draw)
 function buttonPress(){
     // up arrow
     if (hand.x >= 506 && hand.x <= 593 && hand.y >=251 && hand.y <= 300 && display.y > 0){
@@ -188,10 +174,13 @@ function buttonPress(){
         button.frown = true;
     }
 }
+
+//using the button booleans, image display either moves or cycles the images, then calls on the blend functions to alter/place images on the right and left
 function imgDisplay(){
-    // let dispY = constrain(150, 0, 1200);
-    // let dispX = constrain(550, 0, 1200);
+    //displays image using i iterator variable, cropping it and placing it in the center with p5's image arguments. display x and y are the values that allow the image to 'move' under the crop.
     image(display.pics[i],293,55, 205, 270, display.x, display.y, 205, 270);
+    display.transfer = display.pics[i].get(display.x, display.y, 205, 270);
+    //start with the up down left right arrows, moving the current center image
     if (button.up == true){
         display.y -= 2;
         console.log(display.y);
@@ -208,10 +197,11 @@ function imgDisplay(){
         display.x += 2;
         console.log(display.x);
     }
+    // then check if one of the face buttons was pressed and act accordingly
     else if(button.smile == true && delay == false){
-        display.transfer = display.pics[i].get(display.x, display.y, 205, 270);
+        
         if(display.rightActive == true){
-            display.right.blend(display.transfer,0,0,205, 270,0,0,205,270, ADD);
+            blendRight();
         }
         else if(display.rightActive == false){
             display.right = display.transfer;
@@ -222,14 +212,78 @@ function imgDisplay(){
         setTimeout(nextPic, 1000);
     }
     else if(button.frown == true && delay == false){
+        if(display.leftActive == true){
+            blendLeft();
+        }
+        else if(display.leftActive == false){
+            display.left = display.transfer;
+        }
+        display.leftActive = true;
         i ++;
         delay = true;
         setTimeout(nextPic, 1000);
     }
 }
-
+//toggle for boolean in delay function, keeps the user from blasting through all the images with a single button press
 function nextPic(){
     delay = false;
 }
+// blends images for right and left side using a random blend mode corresponding to each side
+// this feels like a convoluted way of doing it, but using a string variable wasn't working as an argument in the blend function so here we are ¯\_(-.-)_/¯
+function blendRight(){
+    blendType = int(random(1,5));
+    if (blendType == 1){
+        //takes the image on the right and combines it with the image from the center held in the display.transfer variable, check the p5 image blend reference for details
+        display.right.blend(display.transfer,0,0,205, 270,0,0,205,270, HARD_LIGHT);
+        //if you want to know how the images are being combined then uncomment these console.log lines :-)
+        //console.log("hard light");
+    }
+    else if (blendType == 2){
+        display.right.blend(display.transfer,0,0,205, 270,0,0,205,270, SOFT_LIGHT);
+        //console.log("soft light");
+    }
+    else if (blendType == 3){
+        display.right.blend(display.transfer,0,0,205, 270,0,0,205,270, ADD);
+        //console.log("add");
+    }
+    else if (blendType == 4){
+        display.right.blend(display.transfer,0,0,205, 270,0,0,205,270, LIGHTEST);
+        //console.log("lightest");
+    }
+    else if (blendType == 5){
+        display.right.blend(display.transfer,0,0,205, 270,0,0,205,270, MULTIPLY);
+        //console.log("multiply");
+    }
+}
+//(i know that the right side is biased towards making bright or even all white images, but i kind of like it that way)
 
-
+// same deal. other side. different blend modes.
+function blendLeft(){
+    blendType = int(random(1,5));
+    if (blendType == 1){
+        display.left.blend(display.transfer,0,0,205, 270,0,0,205,270, DARKEST);
+        //console.log("darkest");
+    }
+    else if (blendType == 2){
+        display.left.blend(display.transfer,0,0,205, 270,0,0,205,270, BURN);
+        //console.log("burn");
+    }
+    else if (blendType == 3){
+        display.left.blend(display.transfer,0,0,205, 270,0,0,205,270, DODGE);
+        //console.log("dodge");
+    }
+    else if (blendType == 4){
+        display.left.blend(display.transfer,0,0,205, 270,0,0,205,270, DIFFERENCE);
+        //console.log("difference");
+    }
+    else if (blendType == 5){
+        display.left.blend(display.transfer,0,0,205, 270,0,0,205,270, SCREEN);
+        //console.log("screen");
+    }
+}
+// a nice gallery view of your works :D - draws the end screen then allows the code to break a bit later, making this the last rendered frame
+function finalScreen(){
+    background("#ffffffff");
+    image(display.left, 30, 40, 328, 432);
+    image(display.right, 430, 40, 328, 432);
+}
